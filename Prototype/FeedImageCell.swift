0a9f0adf4ba9ac9610d2dca748c01544e6ea0901
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-18 11:36:12 -0500

    Adds FeedImageCell with storyboard connections.

---
+++ Prototype/FeedImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+}
